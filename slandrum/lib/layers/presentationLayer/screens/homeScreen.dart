29a74948e4dccfe12f420faa570801d642ee3063
Biscode <bisma.tariq98@gmail.com>
2021-03-21 19:52:14 +0500

    helli

--- slandrum/lib/layers/presentationLayer/screens/homeScreen.dart
+++ slandrum/lib/layers/presentationLayer/screens/homeScreen.dart
@@ -8,9 +8,59 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: Colors.green,
+    ),  Container(
+      color: Colors.blue,
+    ),
+    Container(
+      color: Colors.green,
+    )
+  ];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped, // new
+        currentIndex: _currentIndex, // new
+        items: [
+          new BottomNavigationBarItem(
+            icon: Image.asset("asset/family.png",height: 60,width:60),
+          label: "Family",
+          ),
+         new BottomNavigationBarItem(
+            icon: Image.asset("asset/Calendar.png",height: 60,width:60),
+          label: "Calendar",
+          ),
+         new BottomNavigationBarItem(
+            icon: Image.asset("asset/task.png",height: 60,width:60),
+          label: "Task",
+          ),
+            new BottomNavigationBarItem(
+            icon: Image.asset("asset/task.png",height: 60,width:60),
+          label: "Reward",
+          ),
+            new BottomNavigationBarItem(
+            icon: Image.asset("asset/back.png",height: 60,width:60),
+          label: "Back",
+          ),
+        ],
+      ),
       
     );
   }
